273c5712261bdaf2a206c1ab8d40f00abd4668ff
Chang Tan <tanc7@unlv.nevada.edu>
2021-09-14 01:00:50 -0700

    fuck

--- ProcInject/EXOCET-Windows-Proc-Inject.go
+++ ProcInject/EXOCET-Windows-Proc-Inject.go
@@ -1,4 +1,4 @@
-package main
+package ProcInject
 
 import (
 	"crypto/aes"
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir ~/go/src/exocet
-cp -r ./KeyGenerator ~/go/src/exocet
-go get github.com/amenzhinsky/go-memexec
-echo 'Install Complete'
-
